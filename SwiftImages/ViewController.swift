8eedd64dba89951632099e52b44220cbb33dae87
acavanaghww <acavanagh@weddingwire.com>
2014-08-28 15:41:33 -0400

    commit

--- SwiftImages/ViewController.swift
+++ SwiftImages/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let image = UIImage(named: "osusucks.png")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //prints all of the built in filters you can use
+        println(CIFilter.filterNamesInCategory(kCICategoryBuiltIn))
+    }
+    
+    func sepiaImageForImage(image: UIImage, intensity: Double) -> UIImage! {
+        let beginImage = CIImage(image: image)
+        let filter = CIFilter(name: "CIPixellate")
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        let newImage = UIImage(CIImage: filter.outputImage)
+        return newImage
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sepiaPressed(sender: UIButton) {
+        imageView.image = sepiaImageForImage(image, intensity: 0.5)
     }
-
-
+    
+    @IBAction func normalPressed(sender: UIButton) {
+        imageView.image = image
+    }
 }
 
